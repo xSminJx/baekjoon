1a985d7aa63d9c19850643c9aee9d0c0a74032c7
xSminJx <151181757+xSminJx@users.noreply.github.com>
2024-03-02 01:21:51 +0900

    Create S1 2178 : 미로탐색.c
    전에 만들었던걸로 계속 날먹하는데 이게 맞는걸까

---
+++ Silver/S1 2178 : 미로탐색.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int s[100][100], queue[1000000][2], n, m, day = 1, left, right;
+
+void bfs(int a, int b, int d) {
+	int abs[8] = { a,a,a - 1,a + 1,b - 1,b + 1,b,b };
+	for (int i = 0; i < 4; i++) {
+		int x = abs[i], y = abs[i + 4]; //x, y는 체크해야 하는 좌표
+		if (x < n && x >= 0 && y < m && y >= 0) {
+			if (s[x][y] == -1) {
+				s[x][y] = d;
+				queue[right][0] = x, queue[right][1] = y;
+				right++;
+			}
+		}
+	}
+}
+
+void act() {
+	int stc = right;
+	for (int i = left; i < stc; i++) {
+		bfs(queue[i][0], queue[i][1], day);
+		queue[i][0] = queue[i][1] = 0;
+		left++;
+	}
+
+	if (left < right) {
+		day++;
+		act(left, right);
+	}
+	else return;
+}
+
+int main(void) {
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		for (int o = 0; o < m; o++) {
+			scanf("%1d", &s[i][o]);
+			if (s[i][o] == 1) {
+				s[i][o] = -1;
+			}
+		}
+	}
+	queue[right][0] = 0, queue[right][1] = 0;
+	right++;
+
+	act();
+	printf("%d", s[n - 1][m - 1] + 1);
+
+
+	return 0;
+}
