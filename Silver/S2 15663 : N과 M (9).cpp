330d8161cd31d9f7dd2bd5359802f03c56a4e042
xSminJx <151181757+xSminJx@users.noreply.github.com>
2024-07-17 23:23:08 +0900

    Create S2 15663 : N과 M (9).cpp
    날먹날먹

---
+++ Silver/S2 15663 : N과 M (9).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void fun(int n, int m, vector<int> v, string str) {
+	if (m == 0) cout << str << '\n';
+	else for (int i = 0; i < v.size(); i++) {
+		if (i < v.size() - 1 && v[i + 1] == v[i]) continue;
+		vector<int> a = v;
+		a.erase(a.begin() + i);
+		fun(n, m - 1, a, str + to_string(v[i]) + " ");
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	sort(v.begin(), v.end());
+	fun(n, m, v, "");
+	return 0;
+}
