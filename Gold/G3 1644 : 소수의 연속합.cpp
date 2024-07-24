26146c075c6483b787d4886d660e89e9dd9f34ff
xSminJx <151181757+xSminJx@users.noreply.github.com>
2024-07-25 03:38:17 +0900

    Update G3 1644 : 소수의 연속합.cpp
    메모리 덜 처먹고 시간도 덜 처먹게 고쳤다.

--- Gold/G3 1644 : 소수의 연속합.cpp
+++ Gold/G3 1644 : 소수의 연속합.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <queue>
+#include <cmath>
 using namespace std;
-
-int s[4000001];
 
 int main() {
 	int n, cnt = 0;
 	cin >> n;
+	vector<bool> s(n + 1);
 	vector<long long> v(1);
 
 	s[0] = s[1] = 1;
-	for (int i = 2; i <= 2000; i++) {
+	for (int i = 2; i <= (int)sqrt(n); i++) {
 		if (s[i] == 0) {
 			int k = i;
-			while (k + i <= 4000000) {
+			while (k + i <= n) {
 				k += i;
 				s[k] = 1;
 			}
 		}
 	}
 
-	for (int i = 0; i < 4000000; i++) {
+	for (int i = 0; i <= n; i++) {
 		if (s[i] == 0) v.push_back(i + v.back());
 	}
 
